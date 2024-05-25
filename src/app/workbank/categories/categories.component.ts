6f179c2109c64214a7a12f92114aadc34adb9b02
Patrycja Biernat <patrycja.biernattt@gmail.com>
2024-05-25 13:42:53 +0200

    Baza słówek - dodawanie kategorii

--- src/app/workbank/categories/categories.component.ts
+++ src/app/workbank/categories/categories.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 import { NgForOf, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [NgIcon, NgForOf, NgIf],
+  imports: [NgIcon, NgForOf, NgIf, FormsModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
   categories = ['Kategoria1'];
+  showNewCategoryInput = false;
+  newCategoryName = '';
+
+  addCategory() {
+    this.categories.push(this.newCategoryName);
+    this.newCategoryName = '';
+    this.showNewCategoryInput = false;
+  }
 }
